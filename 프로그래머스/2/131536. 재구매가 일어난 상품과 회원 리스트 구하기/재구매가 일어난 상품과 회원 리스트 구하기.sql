222496ff41c51a6d851a306a95992b3523c5c3c4
HD152521 <wkdrndydtlr@naver.com>
2025-02-22 10:19:41 +0900

    [level 2] Title: 재구매가 일어난 상품과 회원 리스트 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/131536. 재구매가 일어난 상품과 회원 리스트 구하기/재구매가 일어난 상품과 회원 리스트 구하기.sql
@@ -0,0 +1,9 @@
+select user_id,product_id
+from online_sale
+where online_sale_id in (
+    select online_sale_id
+    from online_sale
+    group by user_id,product_id
+    having count(*)>=2
+)
+order by user_id, product_id desc;
